b8e30e9ff083eb29a27ebba96287fb65034ff7c8
Harenkantou <harenkanto.ramanankoraisina@gmail.com>
2026-06-11 10:22:39 +0300

    suppresion_fausse_structure_backend_SQLite

--- backend/src/main/java/com/newapp/backend/repository/TicketCostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.newapp.backend.repository;
-
-import com.newapp.backend.entity.TicketCost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketCostRepository extends JpaRepository<TicketCost, Long> {
-}
